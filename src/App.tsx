a276bb1af1e6563591199bb213d18e2c7301840e
kkr <fukukake7373@gmail.com>
2023-05-06 15:59:37 +0900

    入力文字列を表示する機能を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [inputValues, setInputValues] = useState([""]);
+  const [gptResponses, setGptResponses] = useState([""]);
+  const [fetchData, setFetchData] = useState([]);
+  // const [chats, setChats] = useState<Chat[]>([]);
+
+  // type Chat = {
+  //   inputValue: string;
+  // };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setInputValues([inputValue, ...inputValues]);
+    setInputValue("");
+    setGptResponses([inputValue.charAt(0), ...gptResponses]);
+
+    const endpointURL = `https://pixabay.com/api/?key=36042035-05f311cedad5b347cfbfe8988&q=apple&image_type=photo&pretty=true`;
+
+    fetch(endpointURL)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data.hits);
+        setFetchData(data.hits);
+        console.log(data.hits[0].id);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input type="text" onChange={(e) => handleChange(e)} />
+        <input type="submit" value="送信" />
+      </form>
+      <ul>
+        {inputValues.map((inputValue, index) => (
+          <>
+            <li>{inputValue}</li>
+            <li>{gptResponses[index]}</li>
+            {/* <li>{fetchData[index]}</li> */}
+          </>
+        ))}
+      </ul>
     </div>
   );
 }
